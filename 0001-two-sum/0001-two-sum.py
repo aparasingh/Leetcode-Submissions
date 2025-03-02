4ea98f12f6d8f9fc322f6d9d4e3a53fd4933e768
Aparajita Singh <apara.singh09@gmail.com>
2025-03-02 01:24:57 +0000

    Time: 2 ms (54.44%), Space: 18.8 MB (62.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,11 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         match = {}
         for i in range(len(nums)):
-            if not bool(match):
-                match[nums[i]] = i
+            value = target - nums[i]
+            if value in match.keys():
+                return [match[value],i]
             else:
-                value = target - nums[i]
-                if value in match.keys():
-                     return [match[value],i]
-                else:
-                     match[nums[i]] = i
+                match[nums[i]] = i
