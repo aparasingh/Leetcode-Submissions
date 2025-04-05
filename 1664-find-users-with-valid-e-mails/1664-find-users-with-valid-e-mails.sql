17ad9d978a703f9f64d5bcb4a3f8ad0f18a651b1
Aparajita Singh <apara.singh09@gmail.com>
2025-04-05 23:57:44 +0100

    Time: 720 ms (84.69%), Space: 0B (100%) - LeetHub

--- 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT *
 FROM Users 
-WHERE mail LIKE '%leetcode.com' and (mail REGEXP '^[A-Za-z][A-Za-z0-9_./-]+@leetcode\.com' OR mail REGEXP '^[A-Za-z]+@leetcode\.com');
+WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_./-]*@leetcode(\\?com)?\\.com$';
