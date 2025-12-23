2a6780cb315855c385bd3c9d70a3ef504201d11f
Aparajita Singh <apara.singh09@gmail.com>
2025-12-23 13:09:22 +0000

    Time: 383 ms (14.88%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -6,4 +6,3 @@ dates AS (
 select DATE_ADD(visited_on, INTERVAL -6 DAY) as min_date, visited_on as max_date FROM Customer group by visited_on
 )
 SELECT A.visited_on, SUM(S.amount) as amount, ROUND(SUM(S.amount)/7, 2) as average_amount FROM sum_amount A JOIN dates D ON (A.visited_on = D.max_date) LEFT JOIN sum_amount S ON (S.visited_on BETWEEN D.min_date and D.max_date) WHERE D.min_date IN (select visited_on FROM Customer) group by A.visited_on ORDER BY A.visited_on ASC;
-;
