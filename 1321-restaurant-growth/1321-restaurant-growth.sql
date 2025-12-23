ec71301a8326b28e117b3796df5683f61305cc30
Aparajita Singh <apara.singh09@gmail.com>
2025-12-23 13:14:33 +0000

    Time: 322 ms (80.66%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -2,7 +2,10 @@
 WITH sum_amount AS (
     select visited_on, 
 SUM(amount) as amount FROM Customer group by visited_on),
-dates AS (
+date_range AS (
 select DATE_ADD(visited_on, INTERVAL -6 DAY) as min_date, visited_on as max_date FROM Customer group by visited_on
+),
+all_date_amounts AS (
+SELECT A.visited_on as visited_on, S.visited_on as amount_date, S.amount as amount FROM sum_amount A JOIN date_range D ON (A.visited_on = D.max_date) LEFT JOIN sum_amount S ON (S.visited_on BETWEEN D.min_date and D.max_date) WHERE D.min_date IN (select visited_on FROM Customer)
 )
-SELECT A.visited_on, SUM(S.amount) as amount, ROUND(SUM(S.amount)/7, 2) as average_amount FROM sum_amount A JOIN dates D ON (A.visited_on = D.max_date) LEFT JOIN sum_amount S ON (S.visited_on BETWEEN D.min_date and D.max_date) WHERE D.min_date IN (select visited_on FROM Customer) group by A.visited_on ORDER BY A.visited_on ASC;
+SELECT A.visited_on, SUM(A.amount) as amount, ROUND(SUM(A.amount)/7, 2) as average_amount FROM all_date_amounts A group by A.visited_on ORDER BY A.visited_on ASC;
