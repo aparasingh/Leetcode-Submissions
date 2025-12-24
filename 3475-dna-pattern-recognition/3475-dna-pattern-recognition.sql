e0b7fb475a5c1e49c27541160281008e4267110f
Aparajita Singh <apara.singh09@gmail.com>
2025-12-24 12:33:22 +0000

    Time: 283 ms (39.03%), Space: 0B (100%) - LeetHub

--- 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
+++ 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
@@ -1,10 +1,8 @@
 # Write your MySQL query statement below
 select
-sample_id,
-dna_sequence,
-species,
+*,
 if(dna_sequence LIKE 'ATG%', 1, 0) as has_start,
 if(dna_sequence REGEXP '(^|)TAA$|TAG$|TGA$', 1, 0) as has_stop, 
 if(dna_sequence LIKE '%ATAT%', 1, 0) as has_atat,
 if(dna_sequence LIKE '%GGG%', 1, 0) as has_ggg
-FROM Samples;
+FROM Samples order by sample_id ASC;
