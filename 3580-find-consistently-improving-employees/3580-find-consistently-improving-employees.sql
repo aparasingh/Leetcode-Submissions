54b9a9b2472440a7d9a1d3da7d4bfe3602c9095c
Aparajita Singh <apara.singh09@gmail.com>
2025-12-26 14:44:16 +0000

    Time: 1325 ms (5.05%), Space: 0B (100%) - LeetHub

---
+++ 3580-find-consistently-improving-employees/3580-find-consistently-improving-employees.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+WITH qualifying_employees AS (
+    select employee_id from performance_reviews group by employee_id having count(review_id) >= 3
+),
+review_ranked AS (
+select review_id,
+employee_id,
+rating,
+review_date,
+ROW_NUMBER() OVER (PARTITION BY employee_id ORDER BY review_date DESC) as rnk FROM performance_reviews where employee_id in (select employee_id from qualifying_employees)
+),
+last_three AS (
+select * FROM review_ranked where rnk <= 3
+),
+ratings AS (
+    select employee_id,
+    max(case when rnk = 3 THEN rating end) as lowest_rating,
+    max(case when rnk = 2 THEN rating end) as middle_rating ,
+    max(case when rnk = 1 THEN rating end) as highest_rating  FROM last_three group by employee_id
+)
+select R.employee_id as employee_id, E.name as name, R.highest_rating - R.lowest_rating as improvement_score from ratings R JOIN employees E ON (R.employee_id = E.employee_id) where R.lowest_rating < R.middle_rating and R.middle_rating < R.highest_rating ORDER BY improvement_score DESC, name ASC;
