118ea445956d7422febc084032d2ba51b43469bc
Aparajita Singh <apara.singh09@gmail.com>
2026-01-06 22:20:30 +0000

    Time: 880 ms (61.83%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -2,4 +2,4 @@
 WITH salary_ranked as (
     select DISTINCT departmentId, salary, DENSE_RANK() OVER (PARTITION BY departmentid order by salary DESC) as rnk FROM Employee
 )
-select d.name as Department, e.name as Employee, e.salary as Salary from Employee e join salary_ranked r on (e.departmentId = r.departmentId and r.salary = e.salary) JOIN Department d on (d.id=e.departmentId) where r.rnk IN (1,2,3)
+select d.name as Department, e.name as Employee, e.salary as Salary from Employee e join salary_ranked r on (e.departmentId = r.departmentId and r.salary = e.salary) JOIN Department d on (d.id=e.departmentId) where r.rnk <= 3;
