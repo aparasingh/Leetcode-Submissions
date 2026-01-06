45775060f93d17691296b3bfd98963a181410d87
Aparajita Singh <apara.singh09@gmail.com>
2026-01-06 22:19:52 +0000

    Time: 871 ms (65.35%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,9 +1,5 @@
 # Write your MySQL query statement below
-WITH salary_by_department as (
-    select departmentId, salary FROM Employee group by departmentId,salary ORDER by departmentId asc,salary DESC
-),
-salary_ranked as (
-    select *,
-    ROW_NUMBER() OVER (PARTITION BY departmentid order by salary DESC) as rnk from salary_by_department
+WITH salary_ranked as (
+    select DISTINCT departmentId, salary, DENSE_RANK() OVER (PARTITION BY departmentid order by salary DESC) as rnk FROM Employee
 )
 select d.name as Department, e.name as Employee, e.salary as Salary from Employee e join salary_ranked r on (e.departmentId = r.departmentId and r.salary = e.salary) JOIN Department d on (d.id=e.departmentId) where r.rnk IN (1,2,3)
