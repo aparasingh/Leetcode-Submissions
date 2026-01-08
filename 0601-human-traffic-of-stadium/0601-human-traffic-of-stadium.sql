c4acb572e5b23d2ff1e91d33529377b9cf12674d
Aparajita Singh <apara.singh09@gmail.com>
2026-01-08 19:41:03 +0000

    Time: 388 ms (20.43%), Space: 0B (100%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -2,7 +2,7 @@
 WITH hundred_visits AS (
     select * from Stadium where people >= 100
 )
-select * FROM hundred_visits where id IN (select s1.id from hundred_visits s1 join hundred_visits s2 on (s1.id + 1 = s2.id) join hundred_visits s3 on (s3.id = s2.id + 1)
+select * FROM Stadium where id IN (select s1.id from hundred_visits s1 join hundred_visits s2 on (s1.id + 1 = s2.id) join hundred_visits s3 on (s3.id = s2.id + 1)
 UNION ALL
 select s2.id from hundred_visits s1 join hundred_visits s2 on (s1.id + 1 = s2.id) join hundred_visits s3 on (s3.id = s2.id + 1)
 UNION ALL
