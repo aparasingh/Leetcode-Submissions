77afdf6d39884fdc33cb5024014f2bdb71a6ab7b
Aparajita Singh <apara.singh09@gmail.com>
2026-01-08 19:40:29 +0000

    Time: 403 ms (13.94%), Space: 0B (100%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+WITH hundred_visits AS (
+    select * from Stadium where people >= 100
+)
+select * FROM hundred_visits where id IN (select s1.id from hundred_visits s1 join hundred_visits s2 on (s1.id + 1 = s2.id) join hundred_visits s3 on (s3.id = s2.id + 1)
+UNION ALL
+select s2.id from hundred_visits s1 join hundred_visits s2 on (s1.id + 1 = s2.id) join hundred_visits s3 on (s3.id = s2.id + 1)
+UNION ALL
+select s3.id from hundred_visits s1 join hundred_visits s2 on (s1.id + 1 = s2.id) join hundred_visits s3 on (s3.id = s2.id + 1)
+)
+ORDER by visit_date ASC
